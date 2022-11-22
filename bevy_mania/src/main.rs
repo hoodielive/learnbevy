7eee0a03eb82fccb2c95f3933fbf81e8d769b626
hoodielive <hoodielive@gmail.com>
2022-11-21 23:05:10 -0500

    I don't know what to type

--- bevy_mania/src/main.rs
+++ bevy_mania/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use bevy::prelude::*;
+
+fn main()
+{
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor 
+        {
+            title: "Osa Meji".to_string(),
+            width: 598.0,
+            height: 676.0,
+            ..Default::default()
+        })
+       .add_plugins(DefaultPlugins)
+       .run();
 }
