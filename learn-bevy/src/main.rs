51ad09ff62025506a35476d56a56a537f293c6c8
hoodielive <hoodielive@gmail.com>
2023-09-15 22:17:34 -0400

    Components.

--- learn-bevy/src/main.rs
+++ learn-bevy/src/main.rs
@@ -9,37 +9,42 @@ fn main() {
 
 pub fn setup(mut commands: Commands) {
     commands.spawn((
-                Person {
-                    name: "Oyeku".to_string(),
-                },
-                Employed {
-                    job: Job::Doctor,
-                }
-            )
-    );
+        Person {
+            name: "Oyeku".to_string(),
+        },
+        Employed { job: Job::Doctor },
+    ));
     commands.spawn((
-            Person {
-                name: "Ogbe".to_string(),
-            },
-            Employed {
-                job: Job::Lawyer,
-            }
-        )
-    );
+        Person {
+            name: "Ogbe".to_string(),
+        },
+        Employed { job: Job::Lawyer },
+    ));
     commands.spawn((
-                Person {
-                    name: "Iwori".to_string(),
-                },
-                Employed {
-                    job: Job::FireFighter,
-                }
-            )
-    );
+        Person {
+            name: "Iwori".to_string(),
+        },
+        Employed {
+            job: Job::FireFighter,
+        },
+    ));
+    commands.spawn((
+        Person {
+            name: "Idi".to_string(),
+        },
+        Employed { job: Job::Doctor },
+    ));
 }
 
 pub fn print_names(person_query: Query<&Person>) {
     for person in person_query.iter() {
         println!("Name: {}", person.name);
+    }
+}
+
+pub fn with_jobs(job_query: Query<&Person>) {
+    for job in job_query.iter() {
+        println!("The Job is: {}", job.name);
     }
 }
 
@@ -50,13 +55,12 @@ pub struct Person {
 
 #[derive(Component)]
 pub struct Employed {
-    pub job: Job 
-
+    pub job: Job,
 }
 
 #[derive(Debug)]
 pub enum Job {
     Doctor,
     FireFighter,
-    Lawyer.
+    Lawyer,
 }
