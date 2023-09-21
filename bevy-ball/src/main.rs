9bf06d4f05bd82503bf12c36c9fb6f34eb9abf79
hoodielive <hoodielive@gmail.com>
2023-09-21 19:25:20 -0400

    In order use PrimaryWindow, you have to use bevy::window::PrimaryWindow;

---
+++ bevy-ball/src/main.rs
@@ -0,0 +1,25 @@
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
+fn main() {
+    App::new().add_plugins(DefaultPlugins).run();
+}
+
+#[derive(Component)]
+struct Player {}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window: &Window = window_query.get_single().unwrap();
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/ball_blue_large.png"),
+            ..default()
+        },
+        Player {},
+    ));
+}
