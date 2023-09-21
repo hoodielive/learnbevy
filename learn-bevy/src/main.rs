326eb9f0cf461e379e96f2d9245091647ea32745
hoodielive <hoodielive@gmail.com>
2023-09-21 18:14:58 -0400

    Update

--- learn-bevy/src/main.rs
+++ learn-bevy/src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     App::new()
         .add_startup_system(setup)
         .add_system(print_names)
+        .add_system(people_with_jobs)
+        .add_system(people_ready_for_hire)
+        .add_system(person_does_job)
         .run();
 }
 
@@ -12,7 +15,6 @@ pub fn setup(mut commands: Commands) {
         Person {
             name: "Oyeku".to_string(),
         },
-        Employed { job: Job::Doctor },
     ));
     commands.spawn((
         Person {
@@ -39,6 +41,35 @@ pub fn setup(mut commands: Commands) {
 pub fn print_names(person_query: Query<&Person>) {
     for person in person_query.iter() {
         println!("Name: {}", person.name);
+    }
+}
+
+pub fn people_with_jobs(
+    person_query: Query<&Person, With<Employed>>
+) {
+    for person in person_query.iter() {
+        println!("{} has a job.", person.name);
+    }
+}
+
+pub fn people_ready_for_hire(
+    person_query: Query<&Person, Without<Employed>>
+) {
+    for person in person_query.iter() {
+        println!("{} is ready for hire", person.name);
+    }
+}
+
+pub fn person_does_job(
+    person_query: Query<(&Person, &Employed)>
+) {
+    for (person, employed) in person_query.iter() {
+        let job_name = match employed.job {
+            Job::Doctor => "Doctor",
+            Job::FireFighter => "Fire Fighter",
+            Job::Lawyer => "Lawyer",
+        };
+        println!("{0} is a {1}.", person.name, job_name);
     }
 }
 
