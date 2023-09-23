16ecd1ed19e29d89209fadd1fdfa71add15daa46
hoodielive <hoodielive@gmail.com>
2023-09-22 23:03:13 -0400

    Okay, gn.

--- bevy-11-explore/src/main.rs
+++ bevy-11-explore/src/main.rs
@@ -37,9 +37,7 @@ fn add_people(mut commands: Commands) {
 #[derive(Resource)]
 struct GreetTimer(Timer);
 
-fn greet_people(
-    time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>    
-) {
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
     if timer.0.tick(time.delta()).just_finished() {
         for name in &query {
             println!("hello {}!", name.0);
